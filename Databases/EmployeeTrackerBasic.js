08a98b8dcefadce0737a72864407b86f211586a9
The Noise <leespaulding25@gmail.com>
2021-04-29 00:49:51 -0400

    stablishing connection with node and d

--- Databases/EmployeeTrackerBasic.js
+++ Databases/EmployeeTrackerBasic.js
@@ -1,3 +1,12 @@
 // Dependencies
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+
+// Establishing connection
+const connection = mysql.createConnection({
+    host:'localhost',
+    port:3306,
+    user:'root',
+    password:'root',
+    database:'employeeTracker_DB',
+});
