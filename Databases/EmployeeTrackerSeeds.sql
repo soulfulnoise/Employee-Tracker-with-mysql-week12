db5b69909f0a4553b2f64f5da7f66484d9c49813
The Noise <leespaulding25@gmail.com>
2021-04-29 09:33:02 -0400

    began adding values to insert into employee table

--- Databases/EmployeeTrackerSeeds.sql
+++ Databases/EmployeeTrackerSeeds.sql
@@ -4,3 +4,17 @@
 
 USE Employee_trackerDB;
 
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+
+VALUES
+("Alvin","Thompson",1,NULL),
+("Jessie","Alverez",15,NULL),
+("Jamila","Gunn",28,NULL),
+("Sharika","Lewis",1,NULL),
+("Deandre","Lewis",1,NULL),
+("Big","Sean",1,NULL),
+("Alvin","Thompson",1,NULL),
+("Alvin","Thompson",1,NULL),
+("Alvin","Thompson",1,NULL),
+("Alvin","Thompson",1,NULL),
+
