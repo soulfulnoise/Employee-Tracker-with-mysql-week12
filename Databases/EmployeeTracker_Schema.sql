6dc080211c6d8e2721df3044ecdba417ff198957
The Noise <leespaulding25@gmail.com>
2021-04-29 00:43:29 -0400

    created an employee table in mysql DB

--- Databases/EmployeeTracker_Schema.sql
+++ Databases/EmployeeTracker_Schema.sql
@@ -1,3 +1,16 @@
 DROP DATABASE IF EXSISTS employeeTracker_DB;
+
+
 -- creating a DataBase
 CREATE DATABASE employeeTracker_DB;
+
+
+-- created employee table and col's
+CREATE TABLE employee(
+ id INT NOT NULL AUTO_INCREMENT,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT NOT NULL,
+manager_id INT ,
+PRIMARY KEY(id)
+);
